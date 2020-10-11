709288cacdb86052067cceb7f1bf3a62b45596f7
tchiteu <matheussantos.contato362@gmail.com>
2020-10-11 18:24:16 -0300

    Alterando de controller pra DAO :poop:

--- src/main/java/api/morebread/rest/Usuarios.java
+++ src/main/java/api/morebread/rest/Usuarios.java
@@ -1,23 +1,29 @@
 package api.morebread.rest;
 
-import javax.ws.rs.GET;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import api.morebread.model.Usuario;
-import api.morebread.controller.UsuariosController;
-
+import api.morebread.jdbc.UsuarioDAO;
 
 @Path("/usuarios")
-public class Usuarios {
-	@GET
+public class Usuarios extends UtilRest {
+	@POST
 	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("/")
-	public static Response busca() {
-		Response retorno = UsuariosController.buscaTodos();
-		
-		return retorno;
+	public Response cadastro(Usuario usuario) {
+		UsuarioDAO usuarioDAO = new UsuarioDAO();
+
+		if(usuarioDAO.create(usuario)) {
+			return this.buildResponse("Teste String Sucesso");
+		}
+		else {
+			return this.buildErrorResponse("Erro ao cadastrar usuário");
+		}
 	}
 }
